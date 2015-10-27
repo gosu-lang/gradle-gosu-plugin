9e4be08e8e4afa8ad50ed124291f68785abb1146
Kyle Moore <github@kylemoore.com>
2015-10-27 14:55:10 -0700

    Refactor to use AbstractGosuPluginSpecification

--- src/test/groovy/org/gosulang/gradle/functional/DefaultGosuSourceSetTest.groovy
+++ src/test/groovy/org/gosulang/gradle/functional/DefaultGosuSourceSetTest.groovy
@@ -3,47 +3,37 @@ package org.gosulang.gradle.functional
 import org.gosulang.gradle.tasks.DefaultGosuSourceSet
 import org.gradle.api.internal.file.DefaultSourceDirectorySet
 import org.gradle.api.internal.file.FileResolver
-import org.gradle.internal.nativeintegration.services.NativeServices
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.rules.TemporaryFolder
 
 import static org.hamcrest.Matchers.*
-import static org.junit.Assert.assertThat
+import static spock.util.matcher.HamcrestSupport.expect
 
-class DefaultGosuSourceSetTest {
+class DefaultGosuSourceSetTest extends AbstractGosuPluginSpecification {
 
-    @Rule
-    public final TemporaryFolder _testProjectDir = new TemporaryFolder()
+    private DefaultGosuSourceSet sourceSet
 
-    @Before
-    void before() {
-        NativeServices.initialize(_testProjectDir.root)
+    def 'verify the default values'() {
+        when:
+        sourceSet = new DefaultGosuSourceSet("<set-display-name>", [resolve: { it as File }] as FileResolver)
+        
+        then:
+        sourceSet.gosu instanceof DefaultSourceDirectorySet
+        expect sourceSet.gosu, emptyIterable()
+        sourceSet.gosu.displayName == '<set-display-name> Gosu source'
+        expect sourceSet.gosu.filter.includes,  equalTo(['**/*.gs', '**/*.gsx', '**/*.gst'] as Set)
+        expect sourceSet.gosu.filter.excludes, empty()
     }
+    
+    def 'can configure Gosu source'() {
+        given:
+        sourceSet = new DefaultGosuSourceSet("<set-display-name>", [resolve: { it as File }] as FileResolver)
 
-    private final DefaultGosuSourceSet sourceSet = new DefaultGosuSourceSet("<set-display-name>", [resolve: { it as File }] as FileResolver)
-
-    @Test
-    public void defaultValues() {
-        assertThat(sourceSet.gosu, instanceOf(DefaultSourceDirectorySet))
-        assertThat(sourceSet.gosu, emptyIterable())
-        assertThat(sourceSet.gosu.displayName, equalTo('<set-display-name> Gosu source'))
-        assertThat(sourceSet.gosu.filter.includes, equalTo(['**/*.gs', '**/*.gsx', '**/*.gst'] as Set))
-        assertThat(sourceSet.gosu.filter.excludes, empty())
-
-//        assertThat(sourceSet.allGosu, instanceOf(DefaultSourceDirectorySet))
-//        assertThat(sourceSet.allGosu, emptyIterable())
-//        assertThat(sourceSet.allGosu.displayName, equalTo('<set-display-name> Gosu source'))
-//        assertThat(sourceSet.allGosu.source, hasItem(sourceSet.gosu))
-//        assertThat(sourceSet.allGosu.filter.includes, equalTo(['**/*.gs', '**/*.gsx', '**/*.gst'] as Set))
-//        assertThat(sourceSet.allGosu.filter.excludes, empty())
-    }
-
-    @Test
-    public void canConfigureGosuSource() {
-        sourceSet.gosu { srcDir 'src/somepathtogosu' }
-        assertThat(sourceSet.gosu.srcDirs, equalTo([new File('src/somepathtogosu').canonicalFile] as Set))
+        when:
+        sourceSet.gosu {
+            srcDir 'src/somepathtogosu'
+        }
+        
+        then:
+        expect sourceSet.gosu.srcDirs, equalTo([new File('src/somepathtogosu').canonicalFile] as Set)
     }
 
 }
