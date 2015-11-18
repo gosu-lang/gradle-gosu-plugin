427d048a8bbd6efb04148b008008f77c05f299dd
Kyle Moore <github@kylemoore.com>
2015-11-18 15:23:35 -0800

    Gosudoc - testing exclusion filter (#7)

--- src/test/groovy/org/gosulang/gradle/functional/gosudoc/GosudocPackageExclusionFilterTest.groovy
+++ src/test/groovy/org/gosulang/gradle/functional/gosudoc/GosudocPackageExclusionFilterTest.groovy
@@ -5,12 +5,12 @@ import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.UnexpectedBuildFailure
 
-class GosudocMultipleSourcesTest extends AbstractGosuPluginSpecification {
+class GosudocPackageExclusionFilterTest extends AbstractGosuPluginSpecification {
 
-    File srcMainGosu
-    File additionalSourceRoot
+    File srcMainGosu, includedPkg1, includedPkg2, excludedPkg
     File simplePogo
     File anotherPogo
+    File excludedPogo
 
     /**
      * super#setup is invoked automatically
@@ -19,54 +19,59 @@ class GosudocMultipleSourcesTest extends AbstractGosuPluginSpecification {
     @Override
     def setup() {
         srcMainGosu = testProjectDir.newFolder('src', 'main', 'gosu')
-        additionalSourceRoot = testProjectDir.newFolder('folder', 'containing', 'POGOs')
 
         //setup sample files
-        simplePogo = new File(srcMainGosu, asPath('normal', 'SimplePogo.gs'))
+        simplePogo = new File(srcMainGosu, asPath('included1', 'SimplePogo.gs'))
         simplePogo.getParentFile().mkdirs()
-        simplePogo << 
+        simplePogo <<
             """
-            package normal
+            package included1
             
             /**
              * This is SimplePogo
              */
             class SimplePogo {}
             """
-        
-        anotherPogo = new File(additionalSourceRoot, asPath('alt', 'AnotherPogo.gs'))
+
+        anotherPogo = new File(srcMainGosu, asPath('included2', 'AnotherPogo.gs'))
         anotherPogo.getParentFile().mkdirs()
         anotherPogo <<
             """
-            package alt
-            
-            uses java.math.BigDecimal
+            package included2
             
             /**
              * This is AnotherPogo...
              */
-            class AnotherPogo {
-                //adding non-default constructors just for fun
-                construct(str : String) {}
-                construct(i : Integer, bd : BigDecimal) {}
-            }
+            class AnotherPogo {}
             """
-        
+
+        excludedPogo = new File(srcMainGosu, asPath('donotwant', 'ExcludedPogo.gs'))
+        excludedPogo.getParentFile().mkdirs()
+        excludedPogo <<
+            """
+            package donotwant
+            
+            /**
+             * This is ExcludedPogo
+             */
+            class ExcludedPogo {}
+            """
+
     }
 
-    def 'generate Gosudoc from multiple source roots'() {
+    /**
+     * Gosudoc extends SourceTask, so we get includes/excludes filtering for free
+     * @return
+     */
+    def 'test pattern-based package exclusion'() {
         given:
         buildScript << getBasicBuildScriptForTesting() +
             """
-            sourceSets {
-                    main {
-                        gosu {
-                            srcDirs = ['src/main/gosu', 'folder/containing/POGOs']
-                        }
-                    }
+            gosudoc {
+                exclude '**/*not*/*'
             }
             """
-        
+
         when:
         GradleRunner runner = GradleRunner.create()
                 .withProjectDir(testProjectDir.root)
@@ -74,23 +79,26 @@ class GosudocMultipleSourcesTest extends AbstractGosuPluginSpecification {
                 .withArguments('gosudoc', '-is')
 
         BuildResult result = runner.build()
-        
+
         then:
         notThrown(UnexpectedBuildFailure)
         result.standardOutput.contains('Generating Documentation')
-        result.standardOutput.contains('normal.SimplePogo - document : true')
-        result.standardOutput.contains('alt.AnotherPogo - document : true')
+        result.standardOutput.contains('included1.SimplePogo - document : true')
+        result.standardOutput.contains('included2.AnotherPogo - document : true')
+        !result.standardOutput.contains('donotwant.ExcludedPogo - document : true')
 
         File gosudocOutputRoot = new File(testProjectDir.root, asPath('build', 'docs', 'gosudoc'))
-        File simplePogoGosudoc = new File(gosudocOutputRoot, asPath('normal', 'normal.SimplePogo.html'))
-        File anotherPogoGosudoc = new File(gosudocOutputRoot, asPath('alt', 'alt.AnotherPogo.html'))
+        File simplePogoGosudoc = new File(gosudocOutputRoot, asPath('included1', 'included1.SimplePogo.html'))
+        File anotherPogoGosudoc = new File(gosudocOutputRoot, asPath('included2', 'included2.AnotherPogo.html'))
+        File excludedPogoGosudoc = new File(gosudocOutputRoot, asPath('donotwant', 'donotwant.ExcludedPogo.html'))
 
         //validate the generated HTML
         simplePogoGosudoc.exists()
         simplePogoGosudoc.readLines().contains('<div class="block">This is SimplePogo</div>')
         anotherPogoGosudoc.exists()
         anotherPogoGosudoc.readLines().contains('<div class="block">This is AnotherPogo...</div>')
-        
-    }
+        !excludedPogoGosudoc.exists()
 
+    }
+    
 }
