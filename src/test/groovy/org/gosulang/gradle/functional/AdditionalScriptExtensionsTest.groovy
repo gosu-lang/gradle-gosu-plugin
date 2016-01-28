64a3b639fe8f8f51adbd1a2037ef60d6045d9284
Kyle Moore <github@kylemoore.com>
2016-01-28 11:05:40 -0800

    #19 Test for non-standard Gosu class file extensions

--- src/test/groovy/org/gosulang/gradle/functional/AdditionalScriptExtensionsTest.groovy
+++ src/test/groovy/org/gosulang/gradle/functional/AdditionalScriptExtensionsTest.groovy
@@ -1,7 +1,112 @@
 package org.gosulang.gradle.functional
 
-/**
- * Created by kmoore on 1/20/16.
- */
-class AdditionalScriptExtensionsTest {
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import spock.lang.Unroll
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+@Unroll
+class AdditionalScriptExtensionsTest extends AbstractGosuPluginSpecification {
+
+    File simplePogo, dummyRuleSet, dummyRule, orderFile, shouldBeIgnored
+
+    /**
+     * super#setup is invoked automatically
+     * @return
+     */
+    def setup() {
+        simplePogo = new File(testProjectDir.newFolder('src', 'main', 'gosu',), 'SimplePogo.gs')
+        File ruleSetFolder = testProjectDir.newFolder('src', 'main', 'config', 'rules', 'DummyRuleSet_dir')
+        File rulesRootFolder = ruleSetFolder.parentFile
+        dummyRuleSet = new File(rulesRootFolder, 'DummyRuleSet.grs')
+        dummyRule = new File(ruleSetFolder, 'DummyRule.gr')
+        orderFile = new File(ruleSetFolder, 'order.txt')
+        shouldBeIgnored = new File(rulesRootFolder, 'ShouldBeIgnored.file')
+    }
+
+    def 'Compile a non-standard extension [Gradle #gradleVersion]'() {
+        given:
+        buildScript << getBasicBuildScriptForTesting() + """
+            sourceSets.main {
+              gosu {
+                srcDir 'src/main/config'
+                filter.include 'rules/**/*.grs', 'rules/**/*.gr'
+              }
+            }
+            task copyRuleMetadata(type: Copy) {
+              from 'src/main/config'
+              into sourceSets.main.output.classesDir
+              include 'rules/**/order.txt'
+            }
+            tasks.processResources.dependsOn(tasks.copyRuleMetadata)
+        """
+
+        simplePogo << """
+            public class SimplePogo {
+              //do nothing
+            }
+        """
+        
+        dummyRuleSet << """
+            package rules
+            
+            class DummyRuleSet {
+            
+              static function invoke(bean : Object) : Object {
+                return invoke( new Object(), bean )
+              }
+
+              static function invoke(exeSession : Object, bean : Object) : Object {
+                return new Object()
+              }
+            }
+        """
+        
+        dummyRule << """
+            package rules.DummyRuleSet_dir
+            
+            internal class DummyRule {
+            
+              static function doCondition(something : Object) : boolean { 
+                return false 
+              }
+              static function doAction(something : Object, action : Object) {
+                //do nothing
+              }
+            }
+        """
+        
+        orderFile << 'DummyRule'
+
+        shouldBeIgnored << 'Dummy file contents which will be ignored by explicit ant selector include/exclude patterns'
+        
+        when:
+        GradleRunner runner = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withPluginClasspath(pluginClasspath)
+                .withArguments('clean', 'classes', '-is')
+                .withGradleVersion(gradleVersion)
+//                .withDebug(true)
+                .forwardOutput() //TODO remove me; too verbose?
+
+        BuildResult result = runner.build()
+
+        then:
+        result.output.contains('Initializing Gosu compiler')
+        result.task(':compileGosu').outcome == SUCCESS
+        result.task(':copyRuleMetadata').outcome == SUCCESS
+        result.task(':classes').outcome == SUCCESS
+
+        //did we actually compile anything?
+        new File(testProjectDir.root, asPath('build', 'classes', 'main', 'SimplePogo.class')).exists()
+        new File(testProjectDir.root, asPath('build', 'classes', 'main', 'rules', 'DummyRuleSet.class')).exists()
+        new File(testProjectDir.root, asPath('build', 'classes', 'main', 'rules', 'DummyRuleSet_dir', 'DummyRule.class')).exists()
+        new File(testProjectDir.root, asPath('build', 'classes', 'main', 'rules', 'DummyRuleSet_dir', 'order.txt')).exists()
+
+        where:
+        gradleVersion << gradleVersionsToTest
+        
+    }
+
 }
