7ef33fd5c843a468087430eef69c99fcf9fc1a94
Kyle Moore <github@kylemoore.com>
2016-09-19 19:21:33 -0700

    Only test with 2.8 and later; update docs

--- src/test/groovy/org/gosulang/gradle/functional/MultiversionTestable.groovy
+++ src/test/groovy/org/gosulang/gradle/functional/MultiversionTestable.groovy
@@ -4,37 +4,15 @@ trait MultiversionTestable {
 
     private final ClassLoader classLoader = this.class.classLoader
     private final URL _gradleVersionResource = classLoader.getResource("gradleVersion.txt")
-    private final URL _fullyTestedVersionsResource = classLoader.getResource("fullyTestedVersions.txt")
-    private final URL _partiallyTestedVersionsResource = classLoader.getResource("partiallyTestedVersions.txt")
-    private final URL _knownIncompatibleVersionsResource = classLoader.getResource("knownIncompatibleVersions.txt")
+    private final URL _testedVersionsResource = classLoader.getResource("testedVersions.txt")
     private final String DELIMITER = ','
 
     String getGradleVersion() {
         return getFirstLineFromResource(_gradleVersionResource)
     }
 
-    String[] getFullyTestedVersions() {
-        return getFirstLineFromResource(_fullyTestedVersionsResource).split(DELIMITER).collect { it.trim() }
-    }
-
-    String[] getPartiallyTestedVersions() {
-        return getFirstLineFromResource(_partiallyTestedVersionsResource).split(DELIMITER).collect { it.trim() }
-    }
-
-    String[] getKnownIncompatibleVersions() {
-        return getFirstLineFromResource(_knownIncompatibleVersionsResource).split(DELIMITER).collect { it.trim() }
-    }
-
-    /**
-     * @return map of gradleVersion -> knownBreak status
-     */
-    Map<String, Boolean> getCompatibilityMap() {
-        Map<String, Boolean> map = new HashMap()
-        getPartiallyTestedVersions().each { map.put(it, false) }
-        getKnownIncompatibleVersions().each { map.put(it, true) }
-        getFullyTestedVersions().each { map.put(it, false) }
-        map.put(getGradleVersion(), false)
-        return map
+    String[] getTestedVersions() {
+        return getFirstLineFromResource(_testedVersionsResource).split(DELIMITER).collect { it.trim() }
     }
 
     @SuppressWarnings("GrMethodMayBeStatic")
