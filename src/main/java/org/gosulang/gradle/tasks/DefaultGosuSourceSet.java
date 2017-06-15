31070337bc1c9966c7dbed7f81603f9aee6ff2a1
Kyle Moore <github@kylemoore.com>
2017-06-15 15:21:52 -0700

    Minimum work to pass a test using new partitioned compiler outputs

--- src/main/java/org/gosulang/gradle/tasks/DefaultGosuSourceSet.java
+++ src/main/java/org/gosulang/gradle/tasks/DefaultGosuSourceSet.java
@@ -14,19 +14,19 @@ public class DefaultGosuSourceSet implements GosuSourceSet {
   private final SourceDirectorySet _gosu;
   private final SourceDirectorySet _allGosu;
 
-  public DefaultGosuSourceSet( String displayName, FileResolver fileResolver ) {
-    _gosu = createSourceDirectorySet(displayName + " Gosu source", fileResolver);
+  public DefaultGosuSourceSet( String name, FileResolver fileResolver ) {
+    _gosu = createSourceDirectorySet("gosu", name + " Gosu source", fileResolver);
     _gosu.getFilter().include("**/*.java", "**/*.gs", "**/*.gsx", "**/*.gst", "**/*.gsp");
-    _allGosu = createSourceDirectorySet(displayName + " Gosu source", fileResolver);
+    _allGosu = createSourceDirectorySet("gosu", name + " Gosu source", fileResolver); //TODO 2 args only?
     _allGosu.getFilter().include("**/*.gs", "**/*.gsx", "**/*.gst", "**/*.gsp");
     _allGosu.source(_gosu);
   }
 
 // Below is incompatible with Gradle versions prior to 2.12
 // TODO Introduce this constructor along with Gradle 4.0, at which point we can do away with the reflection below and stop supporting Gradle 2.x
-//  public DefaultGosuSourceSet( String displayName, SourceDirectorySetFactory sourceDirectorySetFactory ) {
-//    _gosu = sourceDirectorySetFactory.create(displayName + " Gosu source");
-//    _gosu.getFilter().include("**/*.gs", "**/*.gsx", "**/*.gst", "**/*.gsp");
+//  public DefaultGosuSourceSet( String name, SourceDirectorySetFactory sourceDirectorySetFactory ) {
+//    _gosu = sourceDirectorySetFactory.create("gosu", name + " Gosu source");
+//    _gosu.getFilter().include("**/*.java", "**/*.gs", "**/*.gsx", "**/*.gst", "**/*.gsp");
 //  }
   
   @Override
@@ -58,14 +58,14 @@ public class DefaultGosuSourceSet implements GosuSourceSet {
    * 
    * @return API-appropriate instance of a DefaultSourceDirectorySet
    */
-  private DefaultSourceDirectorySet createSourceDirectorySet(String name, FileResolver fileResolver) {
+  private DefaultSourceDirectorySet createSourceDirectorySet(String name, String displayName, FileResolver fileResolver) {
     Constructor<DefaultSourceDirectorySet> ctor;
     try {
       Class<?> dftfInterface = Class.forName("org.gradle.api.internal.file.collections.DirectoryFileTreeFactory");
       Class<?> fileTreeFactory = Class.forName("org.gradle.api.internal.file.collections.DefaultDirectoryFileTreeFactory");
       Object fileTreeFactoryInstance = fileTreeFactory.newInstance();
-      ctor = DefaultSourceDirectorySet.class.getConstructor(String.class, FileResolver.class, dftfInterface);
-      return ctor.newInstance(name, fileResolver, fileTreeFactoryInstance);
+      ctor = DefaultSourceDirectorySet.class.getConstructor(String.class, String.class, FileResolver.class, dftfInterface);
+      return ctor.newInstance(name, displayName, fileResolver, fileTreeFactoryInstance);
     } catch (Exception e) {
       //We're probably using a Gradle version lower than 2.12 now, but we need to reflectively check for new interfaces first
       // Prefer DefaultSourceDirectorySetFactory(
