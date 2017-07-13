15dd97edbed8aab400ed0fa74083745e90f7d5bf
sadheesh <svenkatachalam@guidewire.com>
2017-07-13 16:40:34 -0700

    POC on test parallel-1

--- runTestP.sh
+++ runTestP.sh
@@ -1,2 +1,33 @@
 #!/usr/bin/env bash
+
+str=`grep 'testedVersionsCI' gradle.properties|awk -F "=" '{print $2}'`
+IFS=', ' read -r -a arr <<< "$str"
+
+echo "Array...${arr[@]}"
+size=${#arr[@]}
+
+let nt=${CIRCLE_NODE_TOTAL}
+let ni=${CIRCLE_NODE_INDEX}
+
+if [ `expr $size % $nt` -eq 0 ] ; then
+   index=$(( $size / $nt ))
+else
+   index=$(( ($size/$nt)+1 ))
+fi
+
+echo "index....$index"
+
+if [ $ni -eq 0 ] ; then
+        new=("${arr[@]:0:$index}")
+fi
+if [ $ni -eq $((nt-1)) ] ; then
+        new=("${arr[@]: $(($ni * $index)) : $(($size-1))}")
+else
+        new=("${arr[@]: $(($index * $ni)) :$index }")
+fi
+
+var=$(IFS=','; echo "${new[*]}")
+
+echo "testedVersions=$var">>gradle.properties
+
 ./gradlew test
