c51ff7e8cef17a61e406d3714295efbb424f0076
sadheesh <svenkatachalam@guidewire.com>
2017-10-23 11:45:25 -0700

    Enable workflow and parallel-3

--- runParallel.sh
+++ runParallel.sh
@@ -6,5 +6,4 @@ echo "Current node index....${CIRCLE_NODE_INDEX}"
 testFiles=$(find src/test/groovy/org/gosulang/gradle -name '*.groovy' |
 sort | awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}"|awk -F "/groovy/" '{print $2}'|awk -F "." '{print "--tests "$1}'|tr '/' '.')
 echo $testFiles
-#./gradlew test $testFiles
-./gradlew check $testFiles
+./gradlew test $testFiles
