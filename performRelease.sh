ebaaff5f1d6af85998385b0afd0963bb872a8f74
Kyle Moore <github@kylemoore.com>
2018-04-26 15:53:41 -0700

    Maybe automate release plugin?

--- performRelease.sh
+++ performRelease.sh
@@ -2,6 +2,5 @@
 echo "Wow let's do a release!"
 git config --local user.name "Gosu CI"
 git config --local user.email "gosu.lang.team@gmail.com"
-git commit --allow-empty -m 'Empty commit to test git on Travis'
-git show HEAD
-git push -u origin $TRAVIS_BRANCH
+git remote add origin-release https://${GH_TOKEN}@github.com/gosu-lang/gradle-gosu-plugin.git > /dev/null 2>&1
+./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release
