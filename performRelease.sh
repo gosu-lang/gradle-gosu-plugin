852ac37fcb5391b04d59d05285685a452addc18f
Kyle Moore <github@kylemoore.com>
2018-04-26 15:25:42 -0700

    Do more git stuff

--- performRelease.sh
+++ performRelease.sh
@@ -2,3 +2,6 @@
 echo "Wow let's do a release!"
 git config --local user.name "Gosu CI"
 git config --local user.email "gosu.lang.team@gmail.com"
+git commit --allow-empty -m 'Empty commit to test git on Travis'
+git show HEAD
+git push -u origin $TRAVIS_BRANCH
