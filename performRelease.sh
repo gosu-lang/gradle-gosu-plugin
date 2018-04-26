21122f3da2d01687ed113ac3baf0169d89d22afe
Gosu CI <gosu.lang.team@gmail.com>
2018-04-26 16:48:55 -0700

    Code cleanup. Temporarily removing publishPlugins dependency.

--- performRelease.sh
+++ performRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Wow let's do a release!"
+echo "Performing release."
 git config --local user.name "Gosu CI"
 git config --local user.email "gosu.lang.team@gmail.com"
 git remote add origin-release https://${GH_TOKEN}@github.com/gosu-lang/gradle-gosu-plugin.git > /dev/null 2>&1
-./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release
+./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
