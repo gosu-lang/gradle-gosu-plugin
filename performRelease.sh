75ec6e8ddea31f68e70463b3bfe743200d1964ff
Kyle Moore <github@kylemoore.com>
2018-04-26 17:51:43 -0700

    Temporarily dump remotes

--- performRelease.sh
+++ performRelease.sh
@@ -3,4 +3,5 @@ echo "Performing release."
 git config --local user.name "Gosu CI"
 git config --local user.email "gosu.lang.team@gmail.com"
 git remote add origin-release https://${GH_TOKEN}@github.com/gosu-lang/gradle-gosu-plugin.git > /dev/null 2>&1
-./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
+git remote -v
+//./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
