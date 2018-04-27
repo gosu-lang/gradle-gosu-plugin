ebbcd4d59e31f95f3fbd6f2035aea7414571e92a
Kyle Moore <github@kylemoore.com>
2018-04-27 11:03:56 -0700

    Try build stages + deployments; https://docs.travis-ci.com/user/build-stages/#Build-Stages-and-Deployments

--- performRelease.sh
+++ performRelease.sh
@@ -4,4 +4,4 @@ git config --local user.name "Gosu CI"
 git config --local user.email "gosu.lang.team@gmail.com"
 git remote add origin-release https://${GH_TOKEN}@github.com/gosu-lang/gradle-gosu-plugin.git > /dev/null 2>&1
 git remote -v
-//./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
+#./gradlew release -Prelease.useAutomaticVersion=true -Prelease.git.pushToRemote=origin-release -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
