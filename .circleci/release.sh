11b9ab8f5cc0e67980d026544c882547cd111476
sadheesh <svenkatachalam@guidewire.com>
2021-09-27 14:53:10 -0700

    gradle-gosu-plugin test release-99.9.1

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Release Gosu job"
+git config user.email "gosu.lang.team@gmail.com"
+git config user.name "circleCi-bot"
 ./gradlew release -Pgradle.publish.key=$gradlePublishKey -Pgradle.publish.secret=$gradlePublishSecret
