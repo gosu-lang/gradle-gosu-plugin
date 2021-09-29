56c1f504be2182b72e014e2dcd6bc12c90c60b2c
sadheesh <svenkatachalam@guidewire.com>
2021-09-29 10:57:37 -0700

    gradle-gosu-plugin test release-99.9.2-alpha-v10

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -4,8 +4,8 @@ echo "Release Gosu job"
 git config user.email "gosu.lang.team@gmail.com"
 git config user.name "circleCi-bot"
 git remote add upstream origin ${CIRCLE_BRANCH}
-echo "just an dummy line" >> gradle.properties
-#./gradlew release -Pgradle.publish.key=$gradlePublishKey -Pgradle.publish.secret=$gradlePublishSecret
-git add -u
-git commit -m "Checking test commit works fine from CI to github"
-git push --set-upstream origin ${CIRCLE_BRANCH}
+#echo "just an dummy line" >> gradle.properties
+./gradlew release -Pgradle.publish.key=$gradlePublishKey -Pgradle.publish.secret=$gradlePublishSecret
+#git add -u
+#git commit -m "Checking test commit works fine from CI to github"
+#git push --set-upstream origin ${CIRCLE_BRANCH}
